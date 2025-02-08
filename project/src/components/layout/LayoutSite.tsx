702659e1124092f1f559ebb61d35b3ad43832638
a1dana04 <ajdanazumakmatova@gmail.com>
2025-02-08 17:29:19 +0600

    papka

---
+++ project/src/components/layout/LayoutSite.tsx
@@ -0,0 +1,20 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import scss from "./LayoutSite.module.scss";
+import { FC, ReactNode } from "react";
+
+interface LayoutSiteProps {
+  children: ReactNode;
+}
+
+const LayoutSite: FC<LayoutSiteProps> = ({ children }) => {
+  return (
+    <div className={scss.LayoutSite}>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default LayoutSite;

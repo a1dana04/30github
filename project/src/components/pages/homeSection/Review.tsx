2452fb51e1eb7a5a292674e745a5e00f493d120f
a1dana04 <ajdanazumakmatova@gmail.com>
2025-02-17 17:27:52 +0600

    swipper

--- project/src/components/pages/homeSection/Review.tsx
+++ project/src/components/pages/homeSection/Review.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import kav from "@/assets/img/kavychka.png";
 import foto1 from "@/assets/img/Ellipse1.png";
 import foto2 from "@/assets/img/Ellipse2.png";
 import foto3 from "@/assets/img/Ellipse3.png";
-import Image from "next/image";
-import sass from "../homeSection/Review.module.scss";
+import styles from "../homeSection/Review.module.scss";
 
 const reviews = [
   {
@@ -28,31 +36,68 @@ const reviews = [
     role: "Пациент",
     photo: foto3,
   },
+
+  {
+    date: "17.04.2024",
+    text: "Сервис на высоком уровне! Каждая консультация была информативной, врач всегда уделял достаточно времени и внимания.",
+    name: "Наталья",
+    role: "Пациент",
+    photo: foto3,
+  },
+
+  {
+    date: "17.04.2024",
+    text: "Сервис на высоком уровне! Каждая консультация была информативной, врач всегда уделял достаточно времени и внимания.",
+    name: "Наталья",
+    role: "Пациент",
+    photo: foto3,
+  },
+
+  {
+    date: "17.04.2024",
+    text: "Сервис на высоком уровне! Каждая консультация была информативной, врач всегда уделял достаточно времени и внимания.",
+    name: "Виктория",
+    role: "Пациент",
+    photo: foto1,
+  },
 ];
 
 const Review = () => {
   return (
-    <div className={sass.review1}>
+    <div className={styles.review1}>
       <div className="container">
         <h1>Что говорят наши пациенты</h1>
-        <div className={sass.review}>
+        <Swiper
+          modules={[Pagination, Navigation]}
+          spaceBetween={20}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          navigation
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
           {reviews.map((review, index) => (
-            <div key={index} className={sass.from}>
-              <h6>{review.date}</h6>
-              <p>{review.text}</p>
-              <div className={sass.image}>
-                <Image src={kav} alt="img" />
-              </div>
-              <div className={sass.profil}>
-                <Image src={review.photo} alt="" />
-                <div className={sass.text1}>
-                  <h6>{review.name}</h6>
-                  <span>{review.role}</span>
+            <SwiperSlide key={index}>
+              <div className={styles.from}>
+                <h6>{review.date}</h6>
+                <p>{review.text}</p>
+                <div className={styles.image}>
+                  <Image src={kav} alt="Кавычка" />
+                </div>
+                <div className={styles.profil}>
+                  <Image src={review.photo} alt={review.name} />
+                  <div className={styles.text1}>
+                    <h6>{review.name}</h6>
+                    <span>{review.role}</span>
+                  </div>
                 </div>
               </div>
-            </div>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
       </div>
     </div>
   );
